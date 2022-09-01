775d61e07075a0fd1c03ba291f73e7e78c39801f
AugustoCogollo <93163359+AugustoCogollo@users.noreply.github.com>
2022-08-31 21:45:06 -0500

    Implementation of Finger activity recognition
    - Added a system that checks finger activity based on the Input.Touch datat structure
    - Added a function that writes on the console the finger and its activity

--- Assets/Scripts/InputTouchManager.cs
+++ Assets/Scripts/InputTouchManager.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class InputTouchManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    Touch[] fingers = Input.touches;
     void Update()
     {
-        
+        CheckForFingers(fingers);
     }
+
+    private void LateUpdate()
+    {
+        CheckIfSystemCancelledTrackingFinger(fingers);
+    }
+
+    public void CheckForFingers(Touch[] touches)
+    {
+        foreach(Touch touch in touches)
+        {
+            CheckForFingersTouchingScreen(touch);
+            CheckForFingersMoving(touch);
+            CheckForStationaryFingers(touch);
+            CheckIfAFingerStoppedTouchingScreen(touch);
+        }
+    }
+
+    private void CheckForFingersTouchingScreen(Touch touch)
+    {
+        if (touch.phase == TouchPhase.Began)
+            PrintFingerActivity(touch, " touched the screen");
+    }
+
+    private void CheckForFingersMoving(Touch touch)
+    {
+        if (touch.phase == TouchPhase.Moved)
+            PrintFingerActivity(touch, " is moving");
+    }
+
+    private void CheckForStationaryFingers(Touch touch)
+    {
+        if (touch.phase == TouchPhase.Stationary)
+            PrintFingerActivity(touch, " is not moving");
+    }
+
+    private void CheckIfAFingerStoppedTouchingScreen(Touch touch)
+    {
+        if (touch.phase == TouchPhase.Ended)
+            PrintFingerActivity(touch, " stopped touching the screen");
+    }
+    private void PrintFingerActivity(Touch touch, string log)
+    {
+        Debug.Log(touch + log);
+    }
+
+    private void CheckIfSystemCancelledTrackingFinger(Touch[] touches)
+    {
+        foreach(Touch finger in touches)
+            if (finger.phase == TouchPhase.Canceled)
+                Debug.Log("The system stopped tracking: " + finger);
+    }
+
 }
